5a7438442daa6874c157c4cad409061c44292fa0
김준형 <ghilbut@gmail.com>
2023-08-08 06:13:47 +0900

    Bugfix

--- client/src/pages/[id].tsx
+++ client/src/pages/[id].tsx
@@ -5,14 +5,14 @@ import { trpc } from '~/utils/trpc'
 export default function Home() {
     const router = useRouter();
 
-    const subject = trpc.getSubject.useQuery({ id: parseInt(router.query.id) });
+    const id = (router.query.id || '0').toString()
+    const subject = trpc.getSubject.useQuery({ id: parseInt(id)});
     const list = trpc.listQuestions.useQuery();
-
-    console.log(subject.data);
 
     return (
         <main>
             <h1>Question List</h1>
+            <h2 key={subject.data && subject.data.id}>{subject.data && subject.data.title}</h2>
             <ul>
                 {list.data && list.data.map(q => {
                     return (<li key="{q.id}">{q.question}</li>);
